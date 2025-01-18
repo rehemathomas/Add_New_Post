06c8aacee28b9758b57e67655f392c2d977457c4
rehemathomas <thomasrey850@gmail.com>
2025-01-18 17:32:12 +0300

    Include file stream library for file operations

--- Add New Post.cpp
+++ Add New Post.cpp
@@ -1,5 +1,7 @@
 #include <iostream> // Include standard input-output stream library
 #include <vector> // Include vector library for dynamic arrays
 #include <string> // Include string library for string operations
+#include <fstream> // Include file stream library for file operations
+
 
 
