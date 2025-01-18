51899a8c0c8d0916dd91f18db04e342f4da5180f
rehemathomas <thomasrey850@gmail.com>
2025-01-18 17:31:01 +0300

    Include string library for string operations

--- Add New Post.cpp
+++ Add New Post.cpp
@@ -1,3 +1,5 @@
 #include <iostream> // Include standard input-output stream library
 #include <vector> // Include vector library for dynamic arrays
+#include <string> // Include string library for string operations
+
 
